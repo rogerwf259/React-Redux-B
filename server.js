9dfb87120dbd352cb89d4deba1fac43bfeb4780e
Roger <rogeliomer_2@hotmail.com>
2017-06-01 16:54:21 -0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require('express');
+
+// Create our app
+var app = express();
+const PORT = process.env.PORT || 3000;
+
+
+//Middleware used when deploying to Heroku on a free account
+app.use((req, res, next) => {
+  if (req.headers['x-forwarded-proto'] === 'https') {
+      res.redirect('http://' + req.hostname + req.url);
+  } else {
+      next();
+  }
+});
+
+app.use(express.static('public'));
+
+app.listen(PORT, () => {
+  console.log(`Express server is up on port ${PORT}`);
+});
